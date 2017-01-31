4c917c5389ca6bc6fdda653cf4182711f00b3171
CsrCornejo <cadalcito@gmail.com>
2017-01-30 19:48:04 -0600

    Added js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-HOLA
+function myFunction() {
+    var texto = document.getElementById("exampleTextarea").value;
+    var array = texto.replace(/\n/g, " ").split(" ");
+
+    document.getElementById("demo").innerHTML = array;
+}
