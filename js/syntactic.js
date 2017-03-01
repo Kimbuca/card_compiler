bcd33129c4077c026e65606243a8aaf92b89f200
audray <arichan.hr16@gmail.com>
2017-02-28 21:15:19 -0600

    Missing just customer function

--- js/syntactic.js
+++ js/syntactic.js
@@ -50,10 +50,75 @@ function error(str){
 	toastr.error('Error in compilation: ' + str);
 };
 
+function verificar_number(){
+	return !isNaN(tokens[index]); 
+}
+
 function functions(){
 
 }
 
 function main_function(){
 	
+}
+
+function expression(){
+	if(verificar("if")){
+		if_expression();
+	}
+	else if(verificar("while")){
+		while_expression();
+	}else if(verificar("iterate"){
+		iterate_expression();
+	}else{
+		call_function();
+	}
+}
+
+function call_function(){
+	name_of_function();
+}
+
+function name_of_function(){
+	if(verificar("flip") | verificar("getCard") | verificar("putCard")){
+		official_function();
+	}else{
+		customer_function();
+	}
+}
+
+function official_function(){
+	if(exigir("flip")){
+		if(exigir("(")){
+			if(! exigir(")")){
+				throw "')'";
+			}else{
+				flip();
+			}
+		}else{
+				throw "'('";
+		}
+	}else if(exigir("getCard") | exigir("putCard")){
+		if(exigir("(")){
+			if(verificar_number()){
+				if(number_of_deck()){
+					if(!exigir(")")){
+						throw "')'";
+					}
+				}else{
+					throw "'Number between 0 - 52'"
+				}
+			}else{
+				throw "'number'";
+			}
+		}else{
+			throw "'('";
+		}
+	}else{
+		throw "'Function'";
+	}
+}
+
+function customer_function(){
+  
 }
