a5c81d04e968cf4d054983d6a0e54979fc774006
CsrCornejo <cadalcito@gmail.com>
2017-02-28 19:39:57 -0600

    Added some functions

--- js/syntactic.js
+++ js/syntactic.js
@@ -1,6 +1,45 @@
 function syntactic_analysis(tokens){
 
 	console.log(tokens);
+	
+	//Guardar tokens para que accedan todos los demas
 
+	program();
 
+};
+
+function program(tokens){
+	  if ( exigir("class") ) {
+	    if ( exigir("program") ) {
+	      if ( exigir("{") ) {
+		   	 functions();
+		   	 main_function();
+		   	 if ( !exigir("}") ) {
+		   	   error("Expected }");
+		   	 }
+	      }
+	      else {
+	    	error("Expected {");
+	      }
+	    }
+	    else {
+	      error("Expected program");
+	    }
+	  }
+	  else {
+	 	error("Expected class");
+	  }
+}
+
+function exigir(str){
+	return true;
+}
+
+function verificar(str){
+	return true;
+
+}
+
+function error(str){
+	toastr.error('Error in compilation: ' + str);
 };
