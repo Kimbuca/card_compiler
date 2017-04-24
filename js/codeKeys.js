3fae634603123873367cd1cb76679550e1d54600
kimbuca <kimbuca.rola@gmail.com>
2017-04-24 15:51:59 -0500

    ayuda

---
+++ js/codeKeys.js
@@ -0,0 +1,44 @@
+const Keys = {
+
+	IF          : 100,
+	WHILE       : 110,
+	ITERATE     : 120,
+	RETURN      : 130,
+	JUMP        : 160,
+	CALL        : 170,
+	RET         : 500,
+	FIN         : 1000,
+
+	//official functions
+	FLIP        : 330,
+	GETCARD     : 310,
+	PUTCARD     : 320,
+
+	//Deck Conditionals
+	ISEMPTY     : 350,
+	ISNOTEMPTY  : 351,
+
+	//Conditionals
+	ISBLACK       : 201,
+	ISRED         : 202,
+	ISHEART       : 203,
+	ISCLUBS       : 204,
+	ISDIAMOND     : 205,
+	ISSPADES      : 206,
+	ISNOTRED      : 207,
+	ISNOTBLACK    : 208,
+	ISNOTHEART    : 209,
+	ISNOTCLUBS    : 210,
+	ISNOTDIAMOND  : 211,
+	ISNOTSPADES   : 212,
+
+	//Operators
+	LESSTHAN       : 401,
+	GREATERTHAN    : 402,
+	LESSOREQUAL    : 403,
+	GREATEROREQUAL : 404,
+	ISEQUAL        : 405,
+	ISNOTEQUAL     : 406,
+
+	END : 1000,
+}
